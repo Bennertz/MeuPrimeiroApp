aa636231a7b4892bb7c34999dae8fd043a737dc2
Cristiano Bennertz <cristiano.bennertz@gmail.com>
2024-03-19 14:53:11 -0300

    Melhoria sugerida pela IDE.

--- app/src/main/java/com/example/meuprimeiroapp/MainActivity.java
+++ app/src/main/java/com/example/meuprimeiroapp/MainActivity.java
@@ -1,15 +1,11 @@
 package com.example.meuprimeiroapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button botaoPrincipal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +13,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Referência ao botão principal
-        botaoPrincipal = findViewById(R.id.button_1);
+        Button botaoPrincipal = findViewById(R.id.button_1);
 
         //Ação do botão para a activity2
-        botaoPrincipal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                startActivity(intent);
-            }
+        botaoPrincipal.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            startActivity(intent);
         });
     }
 }
